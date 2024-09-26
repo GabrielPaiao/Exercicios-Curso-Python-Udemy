6f71300420c4a2eb5cf27abc561b944ecaa3fcbf
Gabriel Paião <bielpaiao8@gmail.com>
2024-09-26 19:45:07 -0300

    first classes

---
+++ Exercicios/secao_3/aula1.py
@@ -0,0 +1,13 @@
+# COMMENTS AND DOCSTRINGS
+
+# Allows to write a comment
+print(1+1) # Front
+# Down
+print(123)
+
+"""
+DocStrings arent comments!!!
+
+This isnt actually a comment as the compiler reads it, but its ok to 
+use it to documentate your code
+"""
---
+++ Exercicios/secao_3/aula2&3.py
@@ -0,0 +1,9 @@
+# PRINTS
+
+print(12, 34) #Output: 12 34
+print(56, 78) #Output: 56, 78
+print(910, 1112, sep="-", end="\n") #Output: 910-1112
+print(910, 1112, sep="-", end="##") #Output: 910-1112##
+print("Luiz \"Otávio\"") #Output: Luiz "Otávio"
+print(r"Luiz \"Otávio\"") #Output: Luiz \"Otávio\"
+print('Luiz "Otavio"') #Output: Luiz "Otavio"
